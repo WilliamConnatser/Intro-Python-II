4c65892b28f32cbd5fdf9efc9d5ff6d906107fa5
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-07-11 18:38:40 -0500

    finished mvp

--- src/adv.py
+++ src/adv.py
@@ -1,27 +1,22 @@
-import random
+from random import randrange
 from room import Room
 from player import Player
+import os
 
 #
 # Dictionary of rooms
 # Declare all the rooms
 #
 rooms = {
-    "outside": Room(name="Outside Cave Entrance", description="North of you, the cave mount beckons"),
+    "outside": Room(name="Outside Cave Entrance", description="North of you, the cave mount beckons", items=["torch","hatchet"]),
 
-    "foyer": Room(name="Foyer", description="""Dim light filters in from the south. Dusty
-passages run north and east."""),
+    "foyer": Room(name="Foyer", description="Dim light filters in from the south. Dusty passages run north and east.", items=["matches","key"]),
 
-    "overlook": Room(name="Grand Overlook", description="""A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    "overlook": Room(name="Grand Overlook", description="A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.", items=["key","gas"]),
 
-    "narrow": Room(name="Narrow Passage", description="""The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    "narrow": Room(name="Narrow Passage", description="The narrow passage bends here from west to north. The smell of gold permeates the air.", items=["gun","knife"]),
 
-    "treasure": Room(name="Treasure Chamber", description="""You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    "treasure": Room(name="Treasure Chamber", description="You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.", items=["gold","bitcoin"]),
 }
 
 #
@@ -46,9 +41,9 @@ map = [
 #
 # Declare Players
 #
-players = {
-    "player1": Player("Player 1", "outside")
-}
+players = [
+    Player("Player 1", "outside")
+]
 
 #
 # Main Game
@@ -57,46 +52,57 @@ class Game:
     def __init__(self,map,rooms,players):
         self.map = map
         self.rooms = rooms
-        self.players = players        
-        self.turn = random.choice(list(players.keys()))
+        self.players = players
+        self.turn = randrange(len(self.players))
         self.loop()
 
     def announce_turn(self):
-        print(f"It's {self.turn}'s turn...")
-
-    def announce_location(self):
         player = self.players[self.turn]
-        print(f"{self.turn} is in the {player.room}")
-        print(rooms[player.room].description)
+        print(f"It's {player.name}'s turn... \n")
+        print(self.rooms[player.room].description + "\n")
 
-    def announce_possibilities(self):
-        possibilities = self.get_possibilities()     
-        print(f"Possible Moves {[direction for direction in possibilities if possibilities[direction] != None]}")
+    def get_possible_moves(self):
+        possible_moves = self.determine_possible_moves()     
+        return f"{[direction for direction in possible_moves if possible_moves[direction] != None]}"
 
     def get_location_index(self):
-        current_location = self.players[self.turn].room;
+        current_location = self.players[self.turn].room
         row_index, column_index = 0, 0
 
         for row in self.map:
-            for column in row:
-                if column == current_location:
-                    break
-                column_index += 1
-            if self.map[column_index][row_index] != None:
+            if current_location in row:
+                column_index = row.index(current_location)
+                row_index = self.map.index(row)
                 break
-            row_index += 1
 
         return {
             "row": row_index,
             "column": column_index
         }
 
-    def get_possibilities(self):
+    def determine_possible_moves(self):
         matrix_index = self.get_location_index()
-        north = self.map[matrix_index["column"] - 1][matrix_index["row"]]
-        east = self.map[matrix_index["column"]][matrix_index["row"] + 1]
-        west = self.map[matrix_index["column"]][matrix_index["row"] - 1]
-        south = self.map[matrix_index["column"] + 1][matrix_index["row"]]
+
+        if matrix_index["row"] - 1 > -1:
+            north = self.map[matrix_index["row"] - 1][matrix_index["column"]]
+        else:
+            north = None
+
+        if matrix_index["column"] + 1 < len(self.map[0]):
+            east = self.map[matrix_index["row"]][matrix_index["column"] + 1]
+        else:
+            east = None
+
+        if matrix_index["column"] - 1 > -1:
+            west = self.map[matrix_index["row"]][matrix_index["column"] - 1]
+        else:
+            west = None
+        
+        if matrix_index["row"] + 1 < len(self.map):
+            south = self.map[matrix_index["row"] + 1][matrix_index["column"]]
+        else:
+            south = None
+        
         return {
             "north": north,
             "east": east,
@@ -104,14 +110,103 @@ class Game:
             "south": south
         }
 
-    def accept_input(self):
-        print("jaa")
+    def parse_input(self):
+        print(f"You can move: {self.get_possible_moves()}")
+        print(f"You can pick up: {self.rooms[self.players[self.turn].room].items}")
+        print(f"Your inventory contains: {self.players[self.turn].inventory}\n")
+        print("1. Type q to quit")
+        print("2. Type in the direction you want to head")
+        print("3. Type in the item you want to pickup")
+        print("4. Type in the item you want to drop\n")
+        action = input(f"What would you like to do? >> ")
+        action = action.strip().lower()
+
+        if action == 'q':
+            exit()
+        elif action in self.rooms[self.players[self.turn].room].items:
+            self.players[self.turn].pickup(action)
+            self.rooms[self.players[self.turn].room].items.remove(action)
+            self.increment_turn()
+            self.loop()
+        elif action in self.players[self.turn].inventory:
+            self.players[self.turn].drop(action)
+            self.rooms[self.players[self.turn].room].items.append(action)
+            self.increment_turn()
+            self.loop()
+        elif self.validate_input(action):
+            self.players[self.turn].move(self.determine_possible_moves()[action])
+            self.increment_turn()
+            self.loop()
+        else:
+            self.loop()
+
+    def validate_input(self, direction):
+        if direction in ["north", "south", "east", "west"] and self.determine_possible_moves()[direction] != None:
+            return True
+        return False
+
+    def increment_turn(self):
+        if self.turn != len(self.players)-1:
+            self.turn += 1
+        else:
+            self.turn = 0
+
+    def print_map(self):
+        space = " "
+        you = "YOU"
+        bottom = "_"
+        top = ""
+
+        for column in self.map[0]:
+            top += f"[{bottom.center(50, '_')}]"
+
+        self.print_centered(top)
+
+        for row in self.map:
+            line1 = ""
+            line2 = ""
+            line3 = ""
+            line4 = ""
+            line5 = ""
+
+            for column in row:
+                if column == None:
+                    line1 += f"[{space.center(50, ' ')}]"
+                    line2 += f"[{space.center(50, ' ')}]"
+                    line3 += f"[{space.center(50, ' ')}]"
+                    line4 += f"[{space.center(50, ' ')}]"
+                    line5 += f"[{bottom.center(50, '_')}]"
+                elif column == self.players[self.turn].room:
+                    line1 += f"[{space.center(50, ' ')}]"
+                    line2 += f"[{rooms[column].name.center(50, ' ')}]"
+                    line3 += f"[{str(rooms[column].items).center(50, ' ')}]"
+                    line4 += f"[{you.center(50, ' ')}]"
+                    line5 += f"[{bottom.center(50, '_')}]"
+                else:
+                    line1 += f"[{space.center(50, ' ')}]"
+                    line2 += f"[{rooms[column].name.center(50, ' ')}]"
+                    line3 += f"[{space.center(50, ' ')}]"
+                    line4 += f"[{space.center(50, ' ')}]"
+                    line5 += f"[{bottom.center(50, '_')}]"
+
+            self.print_centered(line1)
+            self.print_centered(line2)
+            self.print_centered(line3)
+            self.print_centered(line4)
+            self.print_centered(line5)
+
+        print("\n\n")
+
 
     def loop(self):
+        os.system('clear')
+        self.print_map()
         self.announce_turn()
-        self.announce_location()
-        self.announce_possibilities()
+        self.parse_input()
 
+    @staticmethod
+    def print_centered(text):
+        print(text.center(os.get_terminal_size().columns))
 
 game = Game(map,rooms,players)
 
--- src/player.py
+++ src/player.py
@@ -4,3 +4,13 @@ class Player:
     def __init__(self,name,room):
         self.name = name
         self.room = room
+        self.inventory = []
+
+    def move(self,room):
+        self.room = room
+
+    def pickup(self, item):
+        self.inventory.append(item)
+
+    def drop(self, item):
+        self.inventory.remove(item)
