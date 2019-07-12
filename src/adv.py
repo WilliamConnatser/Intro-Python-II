a1739cc791407472a2a76bdc9e56765878518750
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-07-12 16:07:12 -0500

    added ability to generate map with different columns and empty rooms- added id to room objects

--- src/adv.py
+++ src/adv.py
@@ -1,119 +1,80 @@
-from random import randrange
+import random
+import os
 from room import Room
 from player import Player
-import os
 
 #
 # Dictionary of rooms
 # Declare all the rooms
 #
 rooms = {
-    "outside": Room(name="Outside Cave Entrance", description="North of you, the cave mount beckons", items=["torch","hatchet"]),
-
-    "foyer": Room(name="Foyer", description="Dim light filters in from the south. Dusty passages run north and east.", items=["matches","key"]),
-
-    "overlook": Room(name="Grand Overlook", description="A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.", items=["key","gas"]),
-
-    "narrow": Room(name="Narrow Passage", description="The narrow passage bends here from west to north. The smell of gold permeates the air.", items=["gun","knife"]),
-
-    "treasure": Room(name="Treasure Chamber", description="You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.", items=["gold","bitcoin"]),
+    "outside": Room(
+        room_id="outside",
+        name="Outside Cave Entrance",
+        description="North of you, the cave mount beckons",
+        items=["torch","hatchet"]
+    ),
+    "foyer": Room(
+        room_id="foyer",
+        name="Foyer",
+        description="Dim light filters in from the south. Dusty passages run north and east.",
+        items=["matches","key"]
+    ),
+    "overlook": Room(
+        room_id="overlook",
+        name="Grand Overlook",
+        description="A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.",
+        items=["key","gas"]
+    ),
+    "narrow": Room(
+        room_id="narrow", 
+        name="Narrow Passage", 
+        description="The narrow passage bends here from west to north. The smell of gold permeates the air.",
+        items=["gun","knife"]
+    ),
+    "treasure": Room(
+        room_id="treasure", 
+        name="Treasure Chamber",
+        description="You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.",
+        items=["gold","bitcoin"]
+    ),
 }
-
-#
-# Map list matrix
-# Links rooms together
-#
-map = [
-    ["outside", None],
-    ["foyer", "narrow"],
-    ["overlook", "treasure"]
-]
-
-# room['outside'].n_to = room['foyer']
-# room['foyer'].s_to = room['outside']
-# room['foyer'].n_to = room['overlook']
-# room['foyer'].e_to = room['narrow']
-# room['overlook'].s_to = room['foyer']
-# room['narrow'].w_to = room['foyer']
-# room['narrow'].n_to = room['treasure']
-# room['treasure'].s_to = room['narrow']
 
 #
 # Declare Players
 #
 players = [
-    Player("Player 1", "outside")
+    Player("Player 1", "outside"),
+    Player("Player 2", "outside")
 ]
 
 #
 # Main Game
 #
 class Game:
-    def __init__(self,map,rooms,players):
-        self.map = map
-        self.rooms = rooms
-        self.players = players
-        self.turn = randrange(len(self.players))
+    def __init__(self,max_columns,empty_rooms,rooms,players):
+        self.rooms = rooms #Array of Room objects
+        self.max_columns = max_columns #Max columns on matrix
+        self.min_empty_rooms = empty_rooms #Minimum empty rooms
+        self.map = [] #Initialize map matrix
+        self.players = players #Array of Player objects
+
+        self.turn = random.randrange(len(self.players)) #Randomize first turn
+        self.current_player = self.players[self.turn] #Set current player
+
+        self.generate_map() #Generate a randomized 2D Matrix contains the map
+        self.current_room = self.rooms[self.current_player.room] #Set current room
+
         self.loop()
 
     def announce_turn(self):
-        player = self.players[self.turn]
-        print(f"It's {player.name}'s turn... \n")
-        print(self.rooms[player.room].description + "\n")
-
-    def get_possible_moves(self):
-        possible_moves = self.determine_possible_moves()     
-        return f"{[direction for direction in possible_moves if possible_moves[direction] != None]}"
-
-    def get_location_index(self):
-        current_location = self.players[self.turn].room
-        row_index, column_index = 0, 0
-
-        for row in self.map:
-            if current_location in row:
-                column_index = row.index(current_location)
-                row_index = self.map.index(row)
-                break
-
-        return {
-            "row": row_index,
-            "column": column_index
-        }
-
-    def determine_possible_moves(self):
-        matrix_index = self.get_location_index()
-
-        if matrix_index["row"] - 1 > -1:
-            north = self.map[matrix_index["row"] - 1][matrix_index["column"]]
-        else:
-            north = None
-
-        if matrix_index["column"] + 1 < len(self.map[0]):
-            east = self.map[matrix_index["row"]][matrix_index["column"] + 1]
-        else:
-            east = None
-
-        if matrix_index["column"] - 1 > -1:
-            west = self.map[matrix_index["row"]][matrix_index["column"] - 1]
-        else:
-            west = None
-        
-        if matrix_index["row"] + 1 < len(self.map):
-            south = self.map[matrix_index["row"] + 1][matrix_index["column"]]
-        else:
-            south = None
-        
-        return {
-            "north": north,
-            "east": east,
-            "west": west,
-            "south": south
-        }
+        print(f"It's {self.current_player.name}'s turn... \n")
+        print(self.current_room.description + "\n")
 
     def parse_input(self):
-        print(f"You can move: {self.get_possible_moves()}")
-        print(f"You can pick up: {self.rooms[self.players[self.turn].room].items}")
-        print(f"Your inventory contains: {self.players[self.turn].inventory}\n")
+        print(f"You can move: {self.possible_moves()}")
+        print(f"You can pick up: {self.current_room.items}")
+        print(f"Your inventory contains: {self.current_player.inventory}\n")
         print("1. Type q to quit")
         print("2. Type in the direction you want to head")
         print("3. Type in the item you want to pickup")
@@ -123,25 +84,25 @@ class Game:
 
         if action == 'q':
             exit()
-        elif action in self.rooms[self.players[self.turn].room].items:
+        elif action in self.current_room.items:
             self.players[self.turn].pickup(action)
-            self.rooms[self.players[self.turn].room].items.remove(action)
+            self.rooms[self.current_player.room].items.remove(action)
             self.increment_turn()
             self.loop()
-        elif action in self.players[self.turn].inventory:
+        elif action in self.current_player.inventory:
             self.players[self.turn].drop(action)
-            self.rooms[self.players[self.turn].room].items.append(action)
+            self.rooms[self.current_player.room].items.append(action)
             self.increment_turn()
             self.loop()
-        elif self.validate_input(action):
-            self.players[self.turn].move(self.determine_possible_moves()[action])
+        elif self.valid_direction(action):
+            self.players[self.turn].move(self.adjacent_rooms()[action])
             self.increment_turn()
             self.loop()
         else:
             self.loop()
 
-    def validate_input(self, direction):
-        if direction in ["north", "south", "east", "west"] and self.determine_possible_moves()[direction] != None:
+    def valid_direction(self, direction):
+        if direction in ["north", "south", "east", "west"] and self.adjacent_rooms()[direction] != None:
             return True
         return False
 
@@ -151,13 +112,87 @@ class Game:
         else:
             self.turn = 0
 
-    def print_map(self):
+        self.current_player = self.players[self.turn] #Set current player
+        self.current_room = self.rooms[self.current_player.room] #Set current room
+
+    def loop(self):
+        os.system('clear')
+        self.print_map(self.current_player)
+        print("\n\n")
+        self.announce_turn()
+        self.parse_input()
+
+    def generate_map(self):
+
+        rooms = list(self.rooms.keys()) + ([None] * self.min_empty_rooms)
+        if len(rooms) % self.max_columns != 0:
+            rooms = rooms + ([None] * len(rooms) % self.max_columns)
+
+        #Initialize map matrix rows
+        for x in range(len(rooms)//self.max_columns):
+            self.map.append([])
+
+        print(rooms)
+
+        #Insert random rooms into the map matrix columns
+        for x in range(len(self.map)):
+            for banana in range(self.max_columns):
+                rand_index = random.randint(0, len(rooms)-1)
+                self.map[x].append(rooms[rand_index])
+
+                if rooms[rand_index] != None:
+                    #Set row and column on each room object
+                    self.rooms[rooms[rand_index]].row = x
+                    self.rooms[rooms[rand_index]].column = self.map[x].index(rooms[rand_index])
+
+                rooms.remove(rooms[rand_index])
+        print(self.map)
+
+    def possible_moves(self):
+        if not hasattr(self.rooms[self.current_room.id], 'possible_moves'):
+            self.rooms[self.current_room.id].possible_moves = self.adjacent_rooms()
+
+        return f"{[direction for direction in self.rooms[self.current_room.id].possible_moves if self.rooms[self.current_room.id].possible_moves[direction] != None]}"
+
+    def adjacent_rooms(self):
+        if not hasattr(self.rooms[self.current_room.id], 'adjacent_rooms'):
+            print(self.current_room)
+            if self.current_room.row - 1 > -1:
+                north = self.map[self.current_room.row - 1][self.current_room.column]
+            else:
+                north = None
+
+            if self.current_room.column + 1 < len(self.map[0]):
+                east = self.map[self.current_room.row][self.current_room.column + 1]
+            else:
+                east = None
+
+            if self.current_room.column - 1 > -1:
+                west = self.map[self.current_room.row][self.current_room.column - 1]
+            else:
+                west = None
+            
+            if self.current_room.row + 1 < len(self.map):
+                south = self.map[self.current_room.row + 1][self.current_room.column]
+            else:
+                south = None
+        
+            self.rooms[self.current_room.id].adjacent_rooms = {
+                "north": north,
+                "east": east,
+                "west": west,
+                "south": south
+            }
+        
+        return self.rooms[self.current_room.id].adjacent_rooms
+
+    def print_map(self,current_player):
         space = " "
         you = "YOU"
         bottom = "_"
         top = ""
 
-        for column in self.map[0]:
+        for column in range(self.max_columns):
             top += f"[{bottom.center(50, '_')}]"
 
         self.print_centered(top)
@@ -176,7 +211,7 @@ class Game:
                     line3 += f"[{space.center(50, ' ')}]"
                     line4 += f"[{space.center(50, ' ')}]"
                     line5 += f"[{bottom.center(50, '_')}]"
-                elif column == self.players[self.turn].room:
+                elif column == current_player.room:
                     line1 += f"[{space.center(50, ' ')}]"
                     line2 += f"[{rooms[column].name.center(50, ' ')}]"
                     line3 += f"[{str(rooms[column].items).center(50, ' ')}]"
@@ -195,20 +230,11 @@ class Game:
             self.print_centered(line4)
             self.print_centered(line5)
 
-        print("\n\n")
-
-
-    def loop(self):
-        os.system('clear')
-        self.print_map()
-        self.announce_turn()
-        self.parse_input()
-
     @staticmethod
     def print_centered(text):
         print(text.center(os.get_terminal_size().columns))
 
-game = Game(map,rooms,players)
+game = Game(2,1,rooms,players)
 
 # Write a loop that:
 #
